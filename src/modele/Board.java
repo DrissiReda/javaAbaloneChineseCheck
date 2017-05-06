9594d15ed5e1b7d844eabeca6bc3cba172c9df62
DrissiReda <reda.drissi@yahoo.fr>
2017-05-06 15:14:08 +0200

    Constructed board

--- src/modele/Board.java
+++ src/modele/Board.java
@@ -1,32 +1,32 @@
 package modele;
-
+import modele.Config.Color;
 public class Board {
     private Tile GameBoard[][];
 
     public Board(int height,int width) {
         GameBoard = new Tile[height][width];
         for (int j = 0; j < 11; j++)
-            Tile[0][j] = new Tile(((j % 2) == 0 ? ILLEGAL : EMPTY));
+            GameBoard[0][j] = new Tile(((j % 2) == 0 ?Color.ILLEGAL : Color.EMPTY));
         for (int j = 0; j < 11; j++)
-            Tile[height-1][j] = new Tile(((j % 2) == 0 ? ILLEGAL : EMPTY));
+            GameBoard[height-1][j] = new Tile(((j % 2) == 0 ? Color.ILLEGAL : Color.EMPTY));
         for (int i = 5; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                Tile[5-i][j] = new Tile((((i + j) % 2) == 0 ? ILLEGAL : EMPTY));
-                Tile[5-i][width - j-1] = new Tile((((i + j) % 2) == 0 ? ILLEGAL : EMPTY));
+                GameBoard[5-i][j] = new Tile((((i + j) % 2) == 0 ? Color.ILLEGAL : Color.EMPTY));
+                GameBoard[5-i][width - j-1] = new Tile((((i + j) % 2) == 0 ? Color.ILLEGAL : Color.EMPTY));
             }
             for (int j=i;j<width-i-1;j++){
-                Tile[5-i][j] = new Tile((((i + j) % 2) == 0 ? WHITE : EMPTY));
-                Tile[5-i][width - j-1] = new Tile((((i + j) % 2) == 0 ? WHITE : EMPTY));
+                GameBoard[5-i][j] = new Tile((((i + j) % 2) == 0 ? Color.WHITE : Color.EMPTY));
+                GameBoard[5-i][width - j-1] = new Tile((((i + j) % 2) == 0 ? Color.WHITE : Color.EMPTY));
             }
         }
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i; j++) {
-                Tile[5+i][j] = new Tile((((i + j) % 2) == 0 ? ILLEGAL : EMPTY));
-                Tile[5+i][width - j-1] = new Tile((((i + j) % 2) == 0 ? ILLEGAL : EMPTY));
+                GameBoard[5+i][j] = new Tile((((i + j) % 2) == 0 ? Color.ILLEGAL : Color.EMPTY));
+                GameBoard[5+i][width - j-1] = new Tile((((i + j) % 2) == 0 ? Color.ILLEGAL : Color.EMPTY));
             }
             for (int j=i;j<width-i-1;j++){
-                Tile[5+i][j] = new Tile((((i + j) % 2) == 0 ? BLACK : EMPTY));
-                Tile[5+i][width - j-1] = new Tile((((i + j) % 2) == 0 ? BLACK : EMPTY));
+                GameBoard[5+i][j] = new Tile((((i + j) % 2) == 0 ? Color.BLACK : Color.EMPTY));
+                GameBoard[5+i][width - j-1] = new Tile((((i + j) % 2) == 0 ? Color.BLACK : Color.EMPTY));
             }
         }
     }
