c884fb5c65028fd271ae2fa3b4449d0f8ea70c4b
DrissiReda <reda.drissi@yahoo.fr>
2017-05-06 14:29:52 +0200

    Initial Commit

--- src/modele/Board.java
+++ src/modele/Board.java
@@ -1,5 +1,11 @@
 package modele;
 
 public class Board {
+    private Tile GameBoard[][];
+    public Board(){
+        GameBoard = new Tile[11][19];
+        for(int i=0;i<11;i++)
+            for(int j=0;j<19;j++)
 
+    }
 }
